4f12a90caf4b98f57ed4610b637634b7cbcaf9cc
Jasson Alexander Porras <jassonporras@gmail.com>
2019-12-06 13:00:17 -0600

    Improved

---
+++ server/routes/usuario.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const Usuario = require('../models/usuario');
+
+const bcrypt = require('bcrypt');
+
+app.get('/usuario', function(req, res) {
+    res.json('Get Usuario');
+});
+
+// parse application/x-www-form-urlencoded
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+
+    });
+
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    let body = req.body;
+
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+
+module.exports = app;
